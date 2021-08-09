7a51eccdea16c2d31cad476fcd7812c5896fc4d0
mage888 <mage888@naver.com>
2021-08-09 17:46:43 +0900

    Finish 0809 problem set

--- 0809/mage888/연습문제_2/s1.py
+++ 0809/mage888/연습문제_2/s1.py
@@ -10,5 +10,12 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 print(arr)
 
 #1. 행 우선 순회
+for i in range(len(arr)):
+    for j in range(len(arr)+1):
+        print(arr[i][j])
 
 #2. 열 우선 순회
+for i in range(len(arr)+1):
+    for j in range(len(arr)):
+        print(arr[j][i])
+
