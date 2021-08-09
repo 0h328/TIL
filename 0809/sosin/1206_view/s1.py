158439702994a28beae7e51c852433709a6899cb
sosin <svstar94@naver.com>
2021-08-09 19:38:24 +0900

    update annotation

--- 0809/sosin/1206_view/s1.py
+++ 0809/sosin/1206_view/s1.py
@@ -4,16 +4,24 @@ sys.stdin = open('input.txt')
 for T in range(1, 11):
     N = int(input())
     buildings = list(map(int, input().split()))
+    # 조망권이 확보된 수
     result = 0
     for i in range(2, len(buildings)-2):
+        # 현재 높이 저장
         temp = buildings[i]
+        # 앞뒤로 2칸 비교
         for j in range(-2, 3):
+            # 하나라도 클 시 break
             if buildings[i] < buildings[i+j]:
                 break
+            # 같은 건물이면 넘기기
             elif i == i+j:
                 continue
+            # 낮은 층이면 그 층수만큼 빼고 저장
+            # 더 높은 건물이 있으면 그만큼 조망권이 줄어듦으로 min으로 체크
             else:
                 temp = min(temp, buildings[i] - buildings[i+j])
+        # for문 끝나면 현재건물의 조망권이 확보된 수를 더함
         else:
             result += temp
         
--- 0809/sosin/연습문제_4/s1.py
+++ 0809/sosin/연습문제_4/s1.py
@@ -1,18 +1,24 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 재귀함수구현
 def is_babygin(arr, count):
+    # 종료조건
     if count == 2:
         return 1
     else:
+        # 배열의 길이만큼 돈다.
         for i in range(len(arr)):
+            # 만약 현재 위치의 개수가 0보다 클 때
             if arr[i] > 0:
                 # is_triplet
                 if arr[i] >= 3:
+                    # 기존 array를 변경하지 않기 위해 복사 후 처리
                     temp = arr.copy()
                     temp[i] -= 3
+                    # 다음 판단으로 넘김
                     return is_babygin(temp, count+1)
-                # is_run
+                # is_run (배열 길이를 고려해 index=7까지만 체크)
                 if i < 8 and arr[i+1] > 0 and arr[i+2] > 0:
                     temp = arr.copy()
                     temp[i]-=1
