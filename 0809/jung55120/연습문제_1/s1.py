69a9240017be926b85ccc2a4eacb302259ea4943
kellyjung <jung55120@naver.com>
2021-08-09 21:45:10 +0900

    Finish homework 0809

---
+++ 0809/jung55120/연습문제_1/s1.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('input.txt')
+
+num = int(input())
+print(num)
+
+nums = list(map(int, input().split()))
+
+for num in nums :
+    print(num)
+
+
+N = int(input())
+#
+# my_nums = []
+# for _ in range(N) :
+#     nums = list(map(int, input().split()))
+#     my_nums.append(nums)
+
+numbers = [list(map(int, input().split())) for _ in range(N)]
+print(numbers)
