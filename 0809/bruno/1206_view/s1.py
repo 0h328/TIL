20603282d582d0ad620c88043f0cac4e4fe0bc39
bruno <bruno95@naver.com>
2021-08-09 23:56:04 +0900

    secondary commit

--- 0809/bruno/1206_view/s1.py
+++ 0809/bruno/1206_view/s1.py
@@ -1,1 +1,16 @@
-#코드가 들어갈 파일
+import sys
+sys.stdin = open('input.txt')
+
+total_river_view = river_view = 0
+max_side = 0
+for i in range(10):
+    building_h = list(map(int, input().split()))
+    for j in range(2, len(building_h)-2):
+        if building_h[j] > building_h[j-1] and building_h[j] > building_h[j-2] and building_h[j] > building_h[j+1] and building_h[j] > building_h[j+2]:
+            for k in (j-2, j-1, j+1, j+2):
+                if building_h[k] > max_side:
+                    max_side = building_h[k]
+            river_view = building_h[j] - max_side
+            total_river_view += river_view
+
+    print('#{0} {1}'.format(i+1, total_river_view))
