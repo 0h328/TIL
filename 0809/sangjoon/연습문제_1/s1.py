f22cedf457247a3080d85602613e399fccfc98fa
손상준 <sangjoon1019@naver.com>
2021-08-09 12:58:20 +0000

    Update s1.py

--- 0809/sangjoon/연습문제_1/s1.py
+++ 0809/sangjoon/연습문제_1/s1.py
@@ -2,6 +2,11 @@ import sys
 
 sys.stdin = open("input.txt")
 
-num = int(input())
+num1 = int(input())
 
-print(num)
+lst1 = map(int, input().split())
+
+num2 = int(input())
+lst2 = [list(map(int, input().split())) for_ in range(n)]
+
+
