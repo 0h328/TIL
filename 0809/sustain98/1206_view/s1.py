5ece57cddae1894dc3df2b29d0e487748a290efa
sungyeon-0975 <sustain98@naver.com>
2021-08-10 09:43:57 +0900

    1206 수정

--- 0809/sustain98/1206_view/s1.py
+++ 0809/sustain98/1206_view/s1.py
@@ -5,12 +5,12 @@ for t in range(1, 11):
     res = 0
     num = int(input())
     l = list(map(int, input().split())) + [0,0]
-    max_list = [0] + [max(l[i],l[i-1]) for i in range(1,num + 2)]
-    for i in range(2,num):
-        max_height = max(max_list[i-1], max_list[i+2])
+
+    for i in range(2, num):
+        max_height = max(l[i-2], l[i-1], l[i+1], l[i+2])
         if max_height < l[i]:
             res += l[i] - max_height
-    print('#{0} {1}'.format(t,res))
+    print('#{0} {1}'.format(t, res))
 
 
 
