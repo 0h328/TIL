dd5ab4f939ccd22d9cb6f1922e29e5f4e77c834b
sungryul <airtrack03@naver.com>
2021-08-10 19:52:49 +0900

    0810 hw Finished

--- 0810/airtrack03/4831_전기버스/s1.py
+++ 0810/airtrack03/4831_전기버스/s1.py
@@ -10,12 +10,12 @@ for tc in range(1, T+1):
     # M: 충전기 설치된 정류장 수
     K, N, M = map(int, input().split())
     gas_stations = tuple(map(int, input().split()))
-    charged = K
-    ans = 0
-    gas_idx = 0
+    charged = K     # 현재 충전량
+    ans = 0         # 충전 횟수
+    gas_idx = 0     # 현재 위치하거나 다음에 위치한 주유소 idx
 
-    for i in range(1, N+1):
-        charged -= 1
+    for i in range(1, N+1): # 0에서 충전된 상태로 출발하므로 1부터 시작
+        charged -= 1    # 한번 이동할 때마다 충전량 1 감소
         if gas_stations[gas_idx] == i:    # 현 위치가 gas station일 때
             if gas_idx == len(gas_stations) - 1:    # 마지막 gas station일 경우
                 if K < N - i:   # 1회 충전 최대 거리가 남은 거리보다 짧은 경우
@@ -26,16 +26,16 @@ for tc in range(1, T+1):
                 elif K >= N - i:    # 현재 충전량으로는 부족하지만 다시 충전하면 갈 수 있는 경우
                     ans += 1
                     break
-            else:   # 마지막 gas 스테이션 아닌 경우
+            else:   # 마지막 gas 스테이션 아닌 경우, 다음 충전소까지의 거리만 고려
                 if charged < gas_stations[gas_idx+1] - i:   # 이번 충전소에서 충전해야 하는 경우
-                    charged = K
-                    ans += 1
-                gas_idx += 1
+                    charged = K     # 충전하고
+                    ans += 1        # 충전 횟수 1 증가
+                gas_idx += 1        # 주유소 지나가므로 idx 증가
         else:   # 현 위치가 gas station 아닐 때
             if charged >= N - i:    # 충전량으로 남은 거리 다 갈 수 있는 경우
                 break
             else:
-                if charged < gas_stations[0] - i or charged == 0:
+                if charged < gas_stations[0] - i or charged == 0:   # 다음 충전소를 못 가거나 충전량 0이 된 경우
                     ans = 0
                     break
 
--- 0810/airtrack03/4835_구간합/s1.py
+++ 0810/airtrack03/4835_구간합/s1.py
@@ -9,7 +9,7 @@ for tc in range(1, T+1):
     data = tuple(map(int, input().split()))
 
     max_val = 0
-    min_val = 10000 * N     # 최소값 설정: a의 최대값 * 숫자 개수
+    min_val = 10000 * M     # 최소값 설정: a의 최대값 * 숫자 개수
 
     for i in range(N-M+1):
         sum_val = 0
