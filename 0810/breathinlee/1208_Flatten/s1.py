d3b32ddfd95e27a85a0d4d8a83d3439b46081963
breathinlee <dltnals7350@naver.com>
2021-08-10 23:51:44 +0900

    finish 0810 hw

---
+++ 0810/breathinlee/1208_Flatten/s1.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("input.txt")
+
+def max_height():
+    max_value = 0
+    for i in range(100):
+        if box_height[i] > max_value:
+            max_value = box_height[i]
+            max_idx = i
+    return max_idx
+
+def min_height():
+    min_value = 101
+    for i in range(100):
+        if box_height[i] < min_value:
+            min_value = box_height[i]
+            min_idx = i
+    return min_idx
+
+
+for tc in range(1, 11):
+    num = int(input())
+    box_height = list(map(int, input().split()))
+    for i in range(num):
+        box_height[max_height()] -= 1
+        # box_height[box_height.index(max(box_height))] -= 1
+        box_height[min_height()] += 1
+        # box_height[box_height.index(min(box_height))] -= 1
+    height_difference = box_height[max_height()] - box_height[min_height()]
+    # height_difference = box_height[box_height.index(max(box_height))] - box_height[box_height.index(min(box_height))]
+    print('#{} {}'.format(tc, height_difference))
