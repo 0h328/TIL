5b31e6c341ef5124878e329209fd8a6f2da70a0f
mage888 <mage888@naver.com>
2021-08-11 02:42:40 +0900

    goodluck yh

--- 0810/mage888/99_버블정렬/s1.py
+++ 0810/mage888/99_버블정렬/s1.py
@@ -2,13 +2,12 @@ import sys
 sys.stdin = open('input.txt')
 
 T = int(input())
-Numbers = list(map(int,input().split()))
+numbers = list(map(int,input().split()))
 
 
-for i in range(0, T-1):
-# for i in range(len(Numbers)-1, 0, -1):
-    for j in range(i+1, T):
-        if Numbers[i] > Numbers[j]:
-            Numbers[i], Numbers[j] = Numbers[j], Numbers[i]
+for i in range(len(numbers)-1, 0, -1):
+    for j in range(len(numbers)-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 
-print(Numbers)
+print(numbers)
