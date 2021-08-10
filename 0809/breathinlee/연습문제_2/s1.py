42f79e5c59d157e11d95c7d218d657d83c1235b5
breathinlee <dltnals7350@naver.com>
2021-08-10 10:13:35 +0900

    0809hws again

--- 0809/breathinlee/연습문제_2/s1.py
+++ 0809/breathinlee/연습문제_2/s1.py
@@ -7,8 +7,16 @@ N, M = map(int, input(). split())
 
 # 이차원 리스트 받기
 arr = [list(map(int, input(). split())) for _ in range(N)]
-print(arr)
+# print(arr)
 
 # 1. 행 우선 순회
+for i in range(N):
+    for j in range(M):
+        print(arr[i][j], end = ' ')
+    print()
 
 # 2. 열 우선 순회
+for i in range(M):
+    for j in range(N):
+        print(arr[j][i], end = ' ')
+    print()
