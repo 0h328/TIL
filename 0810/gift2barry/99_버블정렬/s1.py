f327d6f157ee7a6cc266133d89afb7186f857fa2
KIM Tae Hun <gift2barry@gmail.com>
2021-08-10 20:41:43 +0900

    0810 hws ver.1

---
+++ 0810/gift2barry/99_버블정렬/s1.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt')
+
+nums = list(map(int, input().split(', ')))
+
+for i in range(0, len(nums)-1):
+    for j in range(0, len(nums)-i-1):
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+print(nums)
