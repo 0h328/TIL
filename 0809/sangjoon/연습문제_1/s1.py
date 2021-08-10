9c492f335c130654b4742ac80a42eb63b25e08a0
onshore <onshorecoding9@gmail.com>
2021-08-10 10:11:48 +0900

    0810: fix error

--- 0809/sangjoon/연습문제_1/s1.py
+++ 0809/sangjoon/연습문제_1/s1.py
@@ -6,4 +6,4 @@ num1 = int(input())
 lst1 = map(int, input().split())
 
 num2 = int(input())
-lst2 = [list(map(int, input().split())) for _ in range(n)]
+lst2 = [list(map(int, input().split())) for _ in range(num2)]
