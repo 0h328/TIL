71bb6a3ea372566a1bb3307656233446551a0cad
unho <djsgh94@naver.com>
2021-08-10 14:17:16 +0900

    0810 first finish

---
+++ 0810/unho/99_버블정렬/s1.py
@@ -0,0 +1,28 @@
+'''
+두개를 쌍으로 이루어서 리스트 오른쪽 끝까지 순환
+두 값을 비교하여 큰 수를 오른쪽으로 옮김
+1번 순환하면 다시 처음부터 반복
+
+시간 복잡도 O(n^2)
+'''
+
+import sys
+sys.stdin = open('input.txt')
+
+# 버블 정렬 함수
+def bubble_sort(li):
+    # 한번 순환시마다 오른쪽에 한번씩 덜 순환해도 됨
+    for i in range(len(li) - 1, -1, -1):
+        # 0번 인덱스부터 i인덱스 이전까지 순환
+        for j in range(0, i):
+            # 오른쪽 값이 더 크면 값 교환
+            if li[j] > li[j + 1]:
+                li[j], li[j + 1] = li[j + 1], li[j]
+
+    return li
+
+
+num_list = list(map(int, input().split()))
+sorted_list = bubble_sort(num_list)
+
+print(sorted_list)
