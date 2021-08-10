5f4fbac94f9452a39f1a072a07709b5d32de7be9
onshore <onshorecoding9@gmail.com>
2021-08-10 13:44:56 +0900

    0810: algorithm afternoon

---
+++ 0810/sangjoon/99_카운팅정렬/s1.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open("input.txt")
+
+nums = list(map(int, input().split()))
+
+# 음수 고려한 리스트 생성
+max_num, min_num = max(nums), min(nums)
+counter = [0] * (max_num - min_num + 1)
+
+for num in nums:
+    counter[num - min_num] += 1
+
+for idx in range(len(counter)):
+    print("{} ".format(idx + min_num) * counter[idx], end="")
