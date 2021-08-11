5890829310589b013d08ff67f9bcc14f8e270fb2
kellyjung <jung55120@naver.com>
2021-08-11 22:36:33 +0900

    Finish Homework 0811

--- 0811/jung55120/1209_Sum/s1.py
+++ 0811/jung55120/1209_Sum/s1.py
@@ -4,9 +4,41 @@ sys.stdin = open('input.txt')
 for _ in range(10) :
     tc = int(input())
 
-    arr = []
-    for i in range(100) :
-        arr.append(list(map(int, input().split())))
+    arr = [list(map(int, input().split())) for _ in range(100)]  # arr에 100개의 input을 받기
 
-    sum(arr[0])
-    print()
+    max_num = 0
+
+    for n in range(100):
+        sum = 0
+        for m in range(100) :
+            sum += arr[n][m]
+            if sum > max_num :
+                max_num = sum
+
+    for n in range(100):
+        sum = 0
+        for m in range(100) :
+            sum += arr[m][n]
+            if sum > max_num :
+                max_num = sum
+
+    for n in range(100):
+        sum = 0
+        for m in range(100) :
+            if n == m :
+                sum += arr[n][m]
+                if sum > max_num :
+                    max_num = sum
+
+    for n in range(99,-1,-1) :
+        sum = 0
+        for m in range(99,-1,-1) :
+            if n == m :
+                sum += arr[m][n]
+                if sum > max_num :
+                    max_num = sum
+
+    print('#{0} {1}'.format(tc, max_num))
+
+
+
