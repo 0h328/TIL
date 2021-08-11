2ec5dff0023b0dd1e25edb4eb3e3b1920e2bcaf9
develohjh <oojh95@naver.com>
2021-08-11 23:06:09 +0900

    0811hws

---
+++ 0811/oojh95/연습문제_3/s1.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+arr = list(map(int, input().split()))
+
+n = len(arr)
+
+for i in range(1 << n):
+    for j in range(n + 1):
+        if i & (1 << j):
+            print(arr[j], end=' ')
+    print()
