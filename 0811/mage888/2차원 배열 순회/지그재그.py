e5167e500cd79a3eaf04db83dcb100109c9da617
mage888 <mage888@naver.com>
2021-08-12 02:51:55 +0900

    practice 0811

--- 0811/mage888/2차원 배열 순회/지그재그.py
+++ 0811/mage888/2차원 배열 순회/지그재그.py
@@ -1,31 +1,40 @@
 import sys
 sys.stdin = open("input.txt")
 
+M = 5
 N = int(input())
 A = [list(map(int,input().split())) for _ in range(N)]
 
-lst = []
+# lst = []
+#
+# for i in range(len(A)):
+#     lst.append(len(A[i]))
+#
+# max_value = lst[0]
+# for i in lst:
+#     if max_value < i:
+#         max_value = i
+#
+# A_list = []
+#
+# for i in range(len(A)):
+#     for j in range(max_value):
+#         try:
+#             if i % 2 == 0:
+#                 A_list.append(A[i][j])
+#             else:
+#                 while N-1-j >= 0:
+#                     A_list.append(A[i][N-1-j])
+#                     break
+#         except IndexError:
+#             pass
+#
+# print(A_list)
 
-for i in range(len(A)):
-    lst.append(len(A[i]))
-
-max_value = lst[0]
-for i in lst:
-    if max_value < i:
-        max_value = i
-
-A_list = []
-
-for i in range(len(A)):
-    for j in range(max_value):
-        try:
-            if i % 2 == 0:
-                A_list.append(A[i][j])
-            else:
-                while N-1-j >= 0:
-                    A_list.append(A[i][N-1-j])
-                    break
-        except IndexError:
-            pass
-
-print(A_list)
+for i in range(N):
+    if i % 2 == 0:
+        for j in range(M):
+            print(A[i][j], end=' ')
+    else:
+        for j in range(M-1,-1,-1):
+            print(A[i][j], end=' ')
