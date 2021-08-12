5a7217490576bd2bce1a0dc06d92a093c9b782f5
onshore <onshorecoding9@gmail.com>
2021-08-12 13:37:21 +0900

    0811: prac 2&3 finish

--- 0811/sangjoon/연습문제_2/s1.py
+++ 0811/sangjoon/연습문제_2/s1.py
@@ -1,9 +1,28 @@
-import sys
+import pathlib, sys
 
-sys.stdin = open("input.txt")
+sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
 
 test_case = int(input())
 
 for test in range(1, test_case + 1):
     ans = 0
+    r = 1
+    c = 1
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+
+    # for i in range(4):
+    #     # print(dr[i], dc[i])
+
+    #     nr = r + dr[i]  # r로부터 dr만큼 이동한 새로운 nr
+    #     nc = c + dc[i]  # c로부터 dc만큼 이동한 새로운 nc
+
+    #     # 1. 벽 안으로 들어오는 경우만 수행
+    #     if (0 <= nr < N) and (0 <= nc < N):
+    #         print(data[nr][nc])
+
+    #     # 2. 벽 밖으로 나가면 그냥 continue
+    #     if nr < 0 or nr >= N or nc < 0 or nc >= N:
+    #         continue
+
     print("#{} {}".format(test, ans))
