755e03353fb4d12200c1f4c0d85b6d7720585e9a
mage888 <mage888@naver.com>
2021-08-13 09:56:32 +0900

    Finish 0812 hw

--- 0812/mage888/1210_Ladder1/s1.py
+++ 0812/mage888/1210_Ladder1/s1.py
@@ -1,3 +1,18 @@
+def search(start):
+    r = 99 # 목적지는 무조건 99번째 행에 있으므로 고정
+    c = start
+
+    while r > 0:            # 맨 윗줄에 도착하기 전까지 위로 올라감
+        r -= 1              # 위로 한 칸 이동
+        if c > 0 and a[r][c-1] == 1:            # 왼쪽으로 사다리를 안 벗어나고, 왼쪽 칸이 1이면
+            while c > 0 and a[r][c-1] == 1:     # 사다리를 벗어나거나, 왼쪽이 1이 아닐때까지 while문 수행
+                c -= 1
+        elif c < 99 and a[r][c+1] == 1:         # 오른쪽으로 사다리를 안 벗어나고, 오른쪽 칸이 1이면
+            while c < 99 and a[r][c+1] == 1:    # 사다리를 벗어나거나 오른쪽이 1이 아닐때까지 while문 수행
+                c += 1
+
+    return c
+
 import sys
 sys.stdin = open('input.txt')
 
@@ -5,17 +20,25 @@ for tc in range(1, 11):
     n = int(input())
     a = [list(map(int,input().split())) for _ in range(100)]
 
-    # 아무리 생각해도 위에서 목적지를 찾는 방법을 못찾겠음.
-    # 그래서, 목적지로부터 출발지를 찾는 방법을 선택.
+    for c in range(100):
+        if a[99][c] == 2:
+            break
 
-    r = 99 # 목적지는 무조건 99번째 행에 있으므로 고정
-    c = ?  # 열이 어디 위치에 있는지 찾는 방법을 모르겠음.
+    # print(c)
 
-    dr = [-1, 0, 0] # r은 위로 갈때 -1, 좌,우로 갈때는 0
-    dc = [0, 1, -1] # c는 위로 갈때 0, 우로 갈때 1, 좌로 갈때 -1
+    ans = search(c)
 
-    while r > 0: # r이 0이 되면 출발지를 찾았으므로 종료
-        nr = r + dr[i] # nr은 r이 배열 내 dr만큼 이동한 곳
-        nc = c + dc[i] # nc는 c가 배열 내 dc만큼 이동한 곳
+    print('#{} {}'.format(tc, ans))
 
-        if nr in range(100) and nc in range(100): # 범위 벗어나는 것을 방지
+
+    # r = 99 # 목적지는 무조건 99번째 행에 있으므로 고정
+    # c = ?  # 열이 어디 위치에 있는지 찾는 방법을 모르겠음.
+    #
+    # dr = [-1, 0, 0] # r은 위로 갈때 -1, 좌,우로 갈때는 0
+    # dc = [0, 1, -1] # c는 위로 갈때 0, 우로 갈때 1, 좌로 갈때 -1
+    #
+    # while r > 0: # r이 0이 되면 출발지를 찾았으므로 종료
+    #     nr = r + dr[i] # nr은 r이 배열 내 dr만큼 이동한 곳
+    #     nc = c + dc[i] # nc는 c가 배열 내 dc만큼 이동한 곳
+    #
+    #     if nr in range(100) and nc in range(100): # 범위 벗어나는 것을 방지
