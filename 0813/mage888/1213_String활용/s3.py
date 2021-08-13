d32090a5fe743ed463b7fea2f3d7a5c3abcbdd06
mage888 <mage888@naver.com>
2021-08-14 01:30:13 +0900

    Finish 0813 hw

---
+++ 0813/mage888/1213_String활용/s3.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt', encoding='UTF-8')
+
+for _ in range(10):
+    n = int(input())
+    p = input()
+    t = input()
+
+    print('#{} {}'.format(n, t.count(p)))
