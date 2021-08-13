2047e90ca5d548f85172c32f3df006faf05a6be4
bruno <bruno95@naver.com>
2021-08-13 16:44:12 +0900

    4843 need to handle edge case

--- 0812/bruno/1210_Ladder1/s1_1210.py
+++ 0812/bruno/1210_Ladder1/s1_1210.py
@@ -5,8 +5,8 @@ for _ in range(10):
     tc = int(input())
     ladder = [list(map(int, input().split())) for _ in range(100)]
 
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
+    dr = [0, 1, 0, -1]                      # 우 하 좌 상
+    dc = [1, 0, -1, 0]                      # 0  1  2  3
 
     for idx in range(len(ladder[99])):      # 도착점의 인덱스 찾기
         if ladder[99][idx] == 2:            # 사다리 마지막 행 탐색
@@ -18,19 +18,19 @@ for _ in range(10):
 
     while button:
 
-        i, j = i + dr[4], j + dc[4]             # 일단 위로 이동
+        i, j = i + dr[3], j + dc[3]                     # 일단 위로 이동
 
-        if ladder[i][j - 1]:                    # 왼쪽에서 1을 만나면
-            while ladder[i][j - 1]:
-                i, j = i + dr[3], j + dc[3]     # 좌측으로 이동
+        if j >= 1 and ladder[i][j - 1]:                 # 왼쪽에서 1을 만나면
+            while j >= 1 and ladder[i][j - 1]:          # 0을 만날때까지
+                i, j = i + dr[2], j + dc[2]             # 좌측으로 이동
 
-        elif ladder[i][j + 1]:                  # 오른쪽에서 1을 만나면
-            while ladder[i][j + 1]:
-                i, j = i + dr[1], j + dc[1]     # 우측으로 이동
+        elif j <= 98 and ladder[i][j + 1]:              # 오른쪽에서 1을 만나면
+            while j <= 98 and ladder[i][j + 1]:         # 0을 만날때까지
+                i, j = i + dr[0], j + dc[0]             # 우측으로 이동
         
-        if i == 0:                              # 맨 윗행에 도착하면
-            start_point = j                     # 열 값 반환
-            button = 0                          # 반복문 종료
+        if i == 0:                      # 맨 윗행에 도착하면
+            start_point = j             # 열 값 반환
+            button = 0                  # 반복문 종료
 
     print('#{} {}'.format(tc, start_point))
 
