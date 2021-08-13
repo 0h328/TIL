6f6c05899122b1b05c17ebf78ab02524fc8f8d19
bruno <bruno95@naver.com>
2021-08-13 16:42:54 +0900

    0811 modified

--- 0811/bruno/연습문제_2/s1_prac2.py
+++ 0811/bruno/연습문제_2/s1_prac2.py
@@ -1,14 +1,22 @@
 import sys
 sys.stdin = open('input.txt')
 
-n = m = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+N = int(input())
+data = [list(map(int, input().split())) for _ in range(N)]
 
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
-k = 0
-print(arr)
-for i in range(n):
-    for j in range(m):
-        ni, nj = i + di[k], i + dj[k]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+r = 1
+c = 1
 
+for i in range(4):
+    nr = r + dr[i]
+    nc = c + dc[i]
+
+    #1. 벽 안으로 들어오는 경우만 수행
+    if (0 <= nr < N) and (0 <= nc < N):
+        print(data[nr][nc])
+
+    #2. 벽 밖으로 나가면 그냥 continue
+    if nr < 0 or nc >= N or nc < 0 or nc >= N:
+        continue
--- 0811/bruno/연습문제_3/s1_prac3.py
+++ 0811/bruno/연습문제_3/s1_prac3.py
@@ -1,21 +1,11 @@
 import sys
 sys.stdin = open('input.txt')
 
-original_set = list(map(int, input().split()))
-bit = [0] * len(original_set)
-for i in range(2):
-    bit[0] = i
-    for j in range(2):
-        bit[1] = j
-        for k in range(2):
-            bit[2] = k
-            for l in range(2):
-                bit[3] = l
-                for m in range(2):
-                    bit[4] = m
-                    for n in range(2):
-                        bit[5] = n
-                        for p in range(len(original_set)):
-                            if bit[p]:
-                                print(original_set[p], end=' ')
-                        print()
+inp = list(map(int, input().split()))
+N = len(inp)
+for i in range(1, 1 << N):
+    for j in range(N):
+        if i & (1 << j):
+            print(inp[j], end=' ')
+    print()
+print() # 공집합
