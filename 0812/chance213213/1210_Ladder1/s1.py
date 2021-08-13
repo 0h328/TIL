db85ec7cba6aa73c3f1a180fcd89e2e6944d868e
jeonghyun <chance213213@gmail.com>
2021-08-13 09:47:47 +0900

    hw finish in s2.py

--- 0812/chance213213/1210_Ladder1/s1.py
+++ 0812/chance213213/1210_Ladder1/s1.py
@@ -7,7 +7,6 @@ dj = [-1, 1, 0]
 for _ in range(10):
     tc = int(input())
     arr = [list(map(int, input().split())) for _ in range(100)]
-    print(arr)
     #2인 자리 찾는 거 검색
     i = 99
     for idx in range(100):
@@ -21,24 +20,32 @@ for _ in range(10):
     cnt_idx_l = 0
     cnt_idx_r = 0
     while cnt < 100:
+        a = cnt_idx_l
+        b = cnt_idx_r
         for k in range(2):
             n_i = i + di[k] #상하좌우 탐색용
             n_j = j + dj[k]
-
-            if arr[n_i][n_j] == 1:  #k=0부터 시작, 왼쪽 1이면, cnt_idx += 1
-                i = n_i             #k=1이면 오른쪽 탐색, 오른쪽 1이면
-                j = n_j
-                cnt_idx_l += (1-k)  #k=0이면 왼쪽인 경우니깐, left에 +1
-                cnt_idx_r += k      #k=1이면 오른쪽인 경우니깐, right에 +1
-            elif k == 0 and n_i == -1:
-                continue
-            else:                   #n_i, n_j
-                n_i -= di[k]
+            if -1<n_i<100 and -1<n_j<100:
+                if arr[n_i][n_j] == 1:  #k=0부터 시작, 왼쪽 1이면, cnt_idx += 1
+                    i = n_i             #k=1이면 오른쪽 탐색, 오른쪽 1이면
+                    j = n_j
+                    cnt_idx_l += (1-k)  #k=0이면 왼쪽인 경우니깐, left에 +1
+                    cnt_idx_r += k      #k=1이면 오른쪽인 경우니깐, right에 +1
+                else:                   #n_i, n_j
+                    n_i -= di[k]
+                    n_j -= dj[k]
+            elif n_j == -1: #좌를 탐색했을 때 0이면, 원래좌표로 돌리고 다시 탐색
+                n_i -= dj[k]
                 n_j -= dj[k]
+            elif n_j == 100: #오른쪽 탐색(k=1)했는데, 범위벗어나면
+                n_i -= dj[k]
+                n_j -= di[k]
+                break
         # 왼쪽&오른쪽 0이면, 위로 올라감, cnt += 1 (99까지 할 거임)
-        i += di[2]
-        j += dj[2]
-        cnt += 1
+        if abs(a-cnt_idx_l)==0 and (b-cnt_idx_r) == 0:
+            i += di[2]
+            j += dj[2]
+            cnt += 1
 
 
     ans = two_idx - cnt_idx_l + cnt_idx_r
