d377be6c103a23d3a5ba41513faa556c7b73fd87
LDJ <dlehdwns1992@naver.com>
2021-08-14 12:30:41 +0900

    Finish hw 0813

---
+++ 0813/ehdwnsdl1210/연습문제_2/s1.py
@@ -0,0 +1,17 @@
+def solve(word):
+    my_word = ''
+    for i in range(len(word)-1, -1, -1):
+        my_word += word[i]
+    return  my_word
+
+import sys
+sys.stdin = open('input.txt')
+
+#1. 반복문 활용
+word = input()
+print(solve(word)) # edcba
+
+#2. pythonic (slicing)
+word2 = input()
+word2 = word2[::-1]
+print(word2) # sgnirts siht esreve
