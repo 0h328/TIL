0b656930df985f229a6eba85e0ead799c6ca13cd
sungyeon-0975 <sustain98@naver.com>
2021-08-14 16:24:33 +0900

    0813 hw complete

--- 0812/sustain98/1210_Ladder1/s1.py
+++ 0812/sustain98/1210_Ladder1/s1.py
@@ -8,7 +8,7 @@ for _ in range(10):
     loc_c = ladders[-1].index(2)
 
     for loc_r in range(99, 0, -1):
-        if loc_c > 0 and ladders[loc_r][loc_c - 1] == 1:
+        if loc_c > 0 and ladders[loc_r][loc_c - 1] == 1:                #다른 방법 없을까....
             while loc_c > 0 and ladders[loc_r][loc_c - 1] == 1:
                 loc_c -= 1
         elif loc_c < 99 and ladders[loc_r][loc_c + 1]:
