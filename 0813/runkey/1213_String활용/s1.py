056b6ecff02cc7afd79e75d0e2ea3e9854e3f4dd
runkey <csi9509@naver.com>
2021-08-17 21:08:09 +0900

    finish 0813 example

--- 0813/runkey/1213_String활용/s1.py
+++ 0813/runkey/1213_String활용/s1.py
@@ -1,4 +1,9 @@
-# import sys
-# sys.stdin = open("input.txt")
+import sys
+sys.stdin = open('input.txt', encoding='UTF8')
 
-
+for _ in range(1, 11):
+    t = input()
+    p = input()
+    m = input()
+    result = m.count(p)
+    print("#{0} {1}".format(t, result))
