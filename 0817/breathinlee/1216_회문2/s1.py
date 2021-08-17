831014163df366f492eb3a355babe320b1c71757
breathinlee <dltnals7350@naver.com>
2021-08-18 01:05:06 +0900

    0817 hw again

--- 0817/breathinlee/1216_회문2/s1.py
+++ 0817/breathinlee/1216_회문2/s1.py
@@ -4,26 +4,29 @@ sys.stdin = open("input.txt")
 for tc in range(1, 11):
     tc = int(input())
     row_arr = [input() for _ in range(100)]
-    col_arr = list(zip(*row_arr))
     max_palindrome = 1
 
+    # 행 고려
+    for i in range(100):                                         # 행
+        for j in range(1, 100):                                  # 회문 길이 범위
+            for k in range(100-j+1):                             # 열
+                for l in range(j//2):
+                    if row_arr[i][k+l] != row_arr[i][k+j-1-l]:
+                        break
+                else:                                            # 회문인 경우
+                    if max_palindrome <= j:
+                        max_palindrome = j
 
-    for i in range(100):
-        for j in range(100-max_palindrome+1):
-            if row_arr[i][j:j+max_palindrome] == row_arr[i][j:j+max_palindrome][::-1]:
-                max_palindrome += 1
-        ret1 = max_palindrome
+    # 열 고려
+    for i in range(100):                                         # 열
+        for j in range(1, 100):                                  # 회문 길이 범위
+            for k in range(100-j+1):                             # 행
+                for l in range(j//2):
+                    if row_arr[k+l][i] != row_arr[k+j-1-l][i]:
+                        break
+                else:                                            # 회문인 경우
+                    if max_palindrome <= j:
+                        max_palindrome = j
 
 
-    for i in range(100):
-        for j in range(100-max_palindrome+1):
-            if col_arr[i][j:j+max_palindrome] == col_arr[i][j:j+max_palindrome][::-1]:
-                max_palindrome += 1
-        ret2 = max_palindrome
-
-    print('#{} {}'.format(tc, max(ret1, ret2)))
-
-
-
-
-
+    print('#{} {}'.format(tc, max_palindrome))
