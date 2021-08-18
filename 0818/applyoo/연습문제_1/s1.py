340b97077b01844b87b2dee51dafe5829997766e
kaser <applyoo@naver.com>
2021-08-18 14:50:56 +0900

    finish 0818 hws

---
+++ 0818/applyoo/연습문제_1/s1.py
@@ -0,0 +1,21 @@
+def push(item):
+    stack.append(item)
+
+
+def pop():
+    return stack.pop()
+
+
+stack = []
+
+print(stack) # []
+push(1)
+push(2)
+push(3)
+print(stack) # [1, 2, 3]
+
+print('---------')
+# 가장 뒤(위)부터 pop
+print(pop()) # 3
+print(pop()) # 2
+print(pop()) # 1
