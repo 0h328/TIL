1e59f469f2d0af8c091a16e6b7ed404a90a476c3
kaser <applyoo@naver.com>
2021-08-19 16:50:37 +0900

    finish 0820 homework

---
+++ 0820/applyoo/5356_의석이의세로로말해요/s1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+T = int(input())
+for test in range(T):
+    arr = []
+    result = ''
+
+    for i in range(5): # 입력을 2차원 리스트로 저장
+        word = list(input())
+        for i in range(15-len(word)): # 빈 칸은 공백으로 채움
+            word.append('')
+        arr.append(word)
+
+    for i in zip(*arr): # 결과 형태로 변환
+        result += ''.join(i)
+
+    print('#{} {}'.format(test+1, result))
