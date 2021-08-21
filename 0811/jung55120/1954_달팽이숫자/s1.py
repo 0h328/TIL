40f27505d03ee4472253e0668f6d81c00bc0cc07
kellyjung <jung55120@naver.com>
2021-08-21 21:24:16 +0900

    Modify 0811

--- 0811/jung55120/1954_달팽이숫자/s1.py
+++ 0811/jung55120/1954_달팽이숫자/s1.py
@@ -1,11 +1,32 @@
 import sys
 sys.stdin = open('input.txt')
 
-T = int(input())
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
 
-for _ in range(T):
-    num = int(input())
-    arr = []
-    for i in range(num) :
-        for j in range(num) :
-            arr.append(arr[i][j])
+T = int(input())
+for tc in range(1, T+1):
+    cnt = 1
+    N = int(input())
+    arr = [[0] * N for i in range(N)]
+    i, j = 0, -1
+    k = 0
+    while cnt <= N*N:
+        ni, nj = i+di[k], j+dj[k]
+        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 0:
+            arr[ni][nj] = cnt
+            cnt += 1
+            i, j = ni, nj
+        else:
+            k = (k+1) % 4
+
+    print('#{}'.format(tc))
+    for i in range(len(arr)):
+        print(*arr[i])
+
+    # for j in range(n):
+    #     for k in range(n-1, -1, -1):
+    #         if stack[j][k] == 0:
+    #             stack[j].pop(k)
+    #
+    #     print(*stack[j])
