dcc982646a97af45259a3a210e53ce5f7f4cdfd0
jeonghyun <chance213213@gmail.com>
2021-08-24 01:35:50 +0900

    0823 hw finished

--- 0823/chance213213/1223_계산기2/s2.py
+++ 0823/chance213213/1223_계산기2/s2.py
@@ -1,11 +1,4 @@
-import sys
-sys.stdin = open('input2.txt')
 
-N = int(input())
-arr = list(input())
-stack = []
-top = -1
-new = []
 
 def push(item):
     global top, stack
@@ -22,30 +15,67 @@ def pop():
         top -= 1
         return ans
 
+def solve(nums):
+    stack = []
+    for num in nums:
+        if num == '+':
+            a = int(stack.pop())
+            b = int(stack.pop())
+            stack.append(a+b)
+        elif num == '*':
+            a = int(stack.pop())
+            b = int(stack.pop())
+            stack.append(a*b)
+        else:
+            stack.append(int(num))
+        # print(stack, num)
+
+    return stack.pop()
 
 
-
-for chr in arr:
-    if chr == '*' or chr == '+':
-        if top == -1 or ord(chr) < ord(stack[top]):
-            push(chr)
-        elif ord(chr) >= ord(stack[top]):
-            new.append(pop())
-    else:  # type(int(chr)) == 'int':
-        new.append(chr)
-
-print('stack:{}'.format(stack))
-print('new:{}'.format(new))
-print('arr:{}'.format(arr))
+import sys
+sys.stdin = open('input.txt')
 
 
-def solve(stack, chr):
-    # if bool(stack) == False: #비어 있을 때
-    if chr == '*' or chr == '+':
-            push()
+for tc in range(1, 11):
+    N = int(input())
+    arr = list(input())
+    stack = []
+    top = -1
+    new = []
 
-    else:# type(int(chr)) == 'int':
-        new.append(chr)
+    for chr in arr:
+        if chr == '*' or chr == '+':
+            if top == -1 or ord(chr) < ord(stack[top]):
+                push(chr)
+            elif ord(chr) >= ord(stack[top]):
+                for _ in range(len(stack)):
+                    if ord(chr) < ord(stack[top]):
+                        break
+                    new.append(pop())
+
+                push(chr)
+
+        else:  # type(int(chr)) == 'int':
+            new.append(chr)
+    else:
+        for _ in range(len(stack)):
+            new.append(stack.pop())
+
+    # print('stack:{}'.format(stack))
+    # print('arr:{}'.format(arr))
+    # print('new:{}'.format(new))
+
+    print('#{} {}'.format(tc, solve(new)))
+    # print(len(arr), len(new))
+#
+# def solve(stack, chr):
+#     # if bool(stack) == False: #비어 있을 때
+#     if chr == '*' or chr == '+':
+#             push()
+#
+#     else:# type(int(chr)) == 'int':
+#         new.append(chr)
     #
     # else:
     #     if top != -1 and stack[top] == '+' and chr == '+':
@@ -62,5 +92,3 @@ def solve(stack, chr):
     #     elif top != -1 and stack[top] == '*' and chr == '*':
     #         pop()
     #         push()
-
-print(arr)
