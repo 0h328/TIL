1dcb0fbffc16b138c41e11c765565f3605186e43
mage888 <mage888@naver.com>
2021-08-23 23:02:37 +0900

    modified 0812

--- 0812/mage888/2001_파리퇴치/s1.py
+++ 0812/mage888/2001_파리퇴치/s1.py
@@ -7,7 +7,8 @@ for tc in range(1, T+1):
     n, m = list(map(int,input().split()))
     a = [list(map(int,input().split())) for _ in range(n)]
 
-    fly_catch = []
+    # fly_catch = []
+    max_value = 0
     for i in range(n-m+1):
         for j in range(n-m+1):
 
@@ -16,11 +17,14 @@ for tc in range(1, T+1):
             for k in range(m):
                 for l in range(m):
                     ans += a[i+k][j+l]
-                    fly_catch.append(ans)
+                    # fly_catch.append(ans)
 
-    max_value = fly_catch[0]
-    for catch in fly_catch:
-        if max_value < catch:
-            max_value = catch
+    # max_value = fly_catch[0]
+    # for catch in fly_catch:
+    #     if max_value < catch:
+    #         max_value = catch
+
+            if max_value < ans:
+                max_value = ans
 
     print('#{} {}'.format(tc, max_value))
