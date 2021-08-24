4c7d12b05c4ce8c1182e78fd590850f630fa4288
sosin <svstar94@naver.com>
2021-08-24 21:32:24 +0900

    0824 algorithm clear

--- 0824/sosin/4874_Forth/s1.py
+++ 0824/sosin/4874_Forth/s1.py
@@ -1,5 +1,35 @@
 # https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVIc7KqfQDFAWg#
 
+import sys
+sys.stdin = open('input.txt')
+
+for T in range(int(input())):
+    nums = []
+    try:
+        for s in input().split():
+            if s == '+':
+                nums.append(nums.pop() + nums.pop())
+            elif s == '*':
+                nums.append(nums.pop() * nums.pop())
+            elif s == '/':
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(b // a)
+            elif s == '-':
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(b - a)
+            elif s == '.':
+                if len(nums) > 1:
+                    error
+                result = nums.pop()
+            else:
+                nums.append(int(s))
+    except:
+        result = 'error'
+
+    print('#{} {}'.format((T+1), result))
+
 #1 84
 #2 error
 #3 168
--- 0824/sosin/4875_미로/s1.py
+++ 0824/sosin/4875_미로/s1.py
@@ -1,4 +1,32 @@
+import sys
+sys.stdin = open('input.txt')
 
+dr = [0, 0, -1, 1]
+dc = [1, -1, 0, 0]
+def dfs(r, c):
+    for k in range(4):
+        nr, nc = r+dr[k], c+dc[k]
+        if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] != 1:
+            if not visited[nr][nc]:
+                visited[nr][nc] = 1
+                dfs(nr, nc)
+
+for T in range(int(input())):
+    result = 0
+    N = int(input())
+    arr = [list(map(int, input())) for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            if arr[r][c] == 2:
+                sr, sc = r, c
+            if arr[r][c] == 3:
+                er, ec = r, c
+
+    visited = [[0] * N for _ in range(N)]
+
+    visited[sr][sc] = 1
+    dfs(sr, sc)
+    print('#{} {}'.format((T+1), visited[er][ec]))
 
 #1 1
 #2 1
