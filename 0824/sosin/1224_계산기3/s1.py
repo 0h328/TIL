d1fc31a608e47774b8b142d58b098e4f39d619a8
sosin <svstar94@naver.com>
2021-08-24 11:23:19 +0900

    0824 problem set

---
+++ 0824/sosin/1224_계산기3/s1.py
@@ -0,0 +1,13 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14tDX6AFgCFAYD&categoryId=AV14tDX6AFgCFAYD&categoryType=CODE&problemTitle=1224&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+
+
+#1 672676
+#2 1974171
+#3 12654
+#4 38756
+#5 4035
+#6 155304
+#7 6964
+#8 2819
+#9 24711
+#10 208785
---
+++ 0824/sosin/4874_Forth/s1.py
@@ -0,0 +1,5 @@
+# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVIc7KqfQDFAWg#
+
+#1 84
+#2 error
+#3 168
