7bf510dff22759bd01fcf71b9ca0ff819b785c3f
edusw <sw200662@naver.com>
2021-08-25 21:13:18 +0900

    0825 hw

--- 0824/sw200662/4880_토너먼트카드게임/s1.py
+++ 0824/sw200662/4880_토너먼트카드게임/s1.py
@@ -1,21 +1,24 @@
 import sys
+
 sys.stdin = open('input.txt')
 
 T = int(input())
 
+
 def whythey(nums):
     if len(nums) == 1:
         return nums
-    middle = (len(nums)-1)//2 + 1
+    middle = (len(nums) - 1) // 2 + 1
     left = nums[:middle]
     right = nums[middle:]
 
     left = whythey(left)
     right = whythey(right)
 
-    return cal(left[0],right[0])
+    return cal(left[0], right[0])
 
-def cal(a,b):
+
+def cal(a, b):
     if card[a] == card[b]:
         return [a]
     elif card[a] == 1:
@@ -34,8 +37,10 @@ def cal(a,b):
         else:
             return [a]
 
+
 for i in range(T):
     N = int(input())
-    card = list(map(int,input().split()))
+    card = list(map(int, input().split()))
     nums = list(range(N))
-    print('#{} {}'.format(i+1,whythey(nums)[0]+1))
+    print('#{} {}'.format(i + 1, whythey(nums)[0] + 1))
+
