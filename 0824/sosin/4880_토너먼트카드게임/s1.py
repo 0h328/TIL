6b12898a97b2f804a45cb1d43d3d13c28ddf8138
sosin <svstar94@naver.com>
2021-08-25 18:01:50 +0900

    0825 algorithm clear

--- 0824/sosin/4880_토너먼트카드게임/s1.py
+++ 0824/sosin/4880_토너먼트카드게임/s1.py
@@ -10,8 +10,8 @@ def check_winner(a, b):
 def binary_rcp(arr, start, end):
     if len(arr) == 1: 
         return (arr[0], start)
-    elif len(arr) == 2: 
-        return (arr[0], start) if check_winner(arr[0], arr[1]) == arr[0] else (arr[1], end)
+    # elif len(arr) == 2: 
+    #     return (arr[0], start) if check_winner(arr[0], arr[1]) == arr[0] else (arr[1], end)
     else:
         mid = (len(arr)-1)//2
         mid_idx = (start+end)//2
