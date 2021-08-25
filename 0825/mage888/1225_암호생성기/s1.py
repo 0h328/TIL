80f567c29bab99f751c1de7f3a29130018173f89
mage888 <mage888@naver.com>
2021-08-25 21:46:17 +0900

    modified 0825

--- 0825/mage888/1225_암호생성기/s1.py
+++ 0825/mage888/1225_암호생성기/s1.py
@@ -7,13 +7,13 @@ for tc in range(1, T+1):
     pw = list(map(int, input().split()))
     M = len(pw)
 
-    k = 0
-    while pw[-1] > 0:
-        k = (k+1) % 6
+    k = 0                               # 차감하기 위해 만든 변수
+    while pw[-1] > 0:                   # pw의 마지막 idx가 0이 되면 종료
+        k = (k+1) % 6                   # k가 0~5까지만 반복하기 위해 만든 식
 
-        if k:
+        if k:                           # k가 0이 아닌, 1~5감소만 해야하므로
             pw.append(pw.pop(0)-k)
 
-    if pw[-1] < 0:
-        pw[-1] = 0
+    if pw[-1] < 0:                      # pw의 마지막 idx가 음수가 되면
+        pw[-1] = 0                      # 0으로 유지
     print('#{} {}'.format(tc, ' '.join(map(str, pw))))
