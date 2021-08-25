a6beb8a3a1e82fc93ac4b7ea2221bc0468d73539
unho <djsgh94@naver.com>
2021-08-25 22:24:00 +0900

    0825 initial

--- 0824/unho/4881_배열최소합/s1.py
+++ 0824/unho/4881_배열최소합/s1.py
@@ -7,7 +7,7 @@ sys.stdin = open('input.txt')
 
 
 
-def solution(i, use_column, tmp_sum):                           # 행 인덱스 / 사용한 열 인덱스 번호 / 현재 단계까지의 합
+def solution(i, tmp_sum):                                       # 행 인덱스 / 사용한 열 인덱스 번호 / 현재 단계까지의 합
     global answer
 
     #Base Case
@@ -18,10 +18,10 @@ def solution(i, use_column, tmp_sum):                           # 행 인덱스 
         return
 
     for c in range(N):                                          # 열 인덱스 반복
-        if c not in use_column:                                 # 현재 열이 사용된 열이 아니면
-            use_column.append(c)                                # 사용열 리스트에 추가
-            solution(i+1, use_column, tmp_sum + board[i][c])    # 재귀함수 (행 인덱스 / 사용한 열 인덱스 리스트 / 현재 단계까지 합)
-            use_column.pop()                                    # 사용한 열 인덱스에서 마지막 열 삭제
+        if not use_column[c]:                                   # 현재 열이 사용된 열이 아니면
+            use_column[c] = 1                                   # 사용열 리스트에 추가
+            solution(i+1, tmp_sum + board[i][c])                # 재귀함수 (행 인덱스 / 사용한 열 인덱스 리스트 / 현재 단계까지 합)
+            use_column[c] = 0                                   # 사용한 열 인덱스에서 마지막 열 삭제
 
 
 
@@ -31,7 +31,8 @@ for tc in range(1, test_case+1):
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
     answer = 9 * N                                              # 현재 최소합 (현재 보드에서 구할수있는 최고합)
+    use_column = [0] * N
 
-    solution(0, [], 0)                                          # 백트래킹 활용 재귀함수 호출
+    solution(0, 0)                                              # 백트래킹 활용 재귀함수 호출
 
     print('#{} {}'.format(tc, answer))
