6d48f04bea716e77625342fc372bd0db5a104452
unho <djsgh94@naver.com>
2021-08-25 23:41:30 +0900

    add

--- 0825/unho/연습문제2_BFS구현/s3.py
+++ 0825/unho/연습문제2_BFS구현/s3.py
@@ -1,6 +1,7 @@
-"""
-1. bfs - 인접 행렬 구현
-"""
+'''
+3. bfs - 인접 딕셔너리 구현
+'''
+
 import queue
 
 
@@ -12,9 +13,8 @@ def bfs(v):
         node = q.get()
         if not visited[node]:
             visited[node] = True
-            for k in range(len(linked)):
-                if linked[node][k] == 1:
-                    q.put(k)
+            for e in linked.get(node):
+                q.put(e)
 
 
 import sys
@@ -24,13 +24,13 @@ sys.stdin = open('input.txt')
 v, e = map(int, input().split())
 
 # 간선 정보 초기화
-linked = [[0]*(v+1) for _ in range(v+1)]
+linked = {}
 
 # Graph 초기화
 in_list = list(map(int, input().split()))
 for i in range(len(in_list)//2):
-    linked[in_list[i * 2]][in_list[i * 2 + 1]] = 1
-    linked[in_list[i * 2 + 1]][in_list[i * 2]] = 1
+    linked[in_list[i*2]] = linked.get(in_list[i*2], []) + [in_list[i*2+1]]
+    linked[in_list[i*2+1]] = linked.get(in_list[i*2+1], []) + [in_list[i*2]]
 
 # 방문 표시 초기화
 visited = [False] * (v+1)
