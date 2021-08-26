f502870a9e9e1152b0b2ae7de99ac9fe6277c270
onshore <onshorecoding9@gmail.com>
2021-08-26 14:15:11 +0900

    0826: finish hw

--- 0826/sangjoon/1226_미로1/s1.py
+++ 0826/sangjoon/1226_미로1/s1.py
@@ -1,25 +1,42 @@
 # 문제 푼 시간
 # 풀이법: Count 사용
-import collections
+from collections import deque
 import pathlib, sys
-
 
 sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
 
 
 def bfs(x: int, y: int):
-    pass
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+    dq = deque([[x, y]])  # 디큐 초기화
+
+    while dq:
+        x, y = dq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if mp[nx][ny] == 3:  # 도착지에 도착했을 경우
+                    return 1
+
+                if mp[nx][ny] == 0:
+                    mp[nx][ny] = 1  # 방문 표시
+                    dq.append([nx, ny])  # 이동 가능할 겨우
+
+    return 0  # 목적지에 도착하지 못할 경우
 
 
-test_case = int(input())
+test_case = 10
 
 for test in range(1, test_case + 1):
-    n = int(input().split())
-    mp = [list(map(int, input().split())) for _ in range(n)]
-
+    test = int(input())
+    n = 16
+    mp = [list(map(int, input())) for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if mp[i][j] == 2:
-                bfs(i, j)
+                ans = bfs(i, j)
 
     print("#{} {}".format(test, ans))
--- 0826/sangjoon/5102_노드의거리/s1.py
+++ 0826/sangjoon/5102_노드의거리/s1.py
@@ -1,15 +1,46 @@
 # 문제 푼 시간
-# 풀이법: Count 사용
+# 풀이법: BFS 사용
 import pathlib, sys
+from collections import deque
 
 sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
+
+
+def bfs(s: int):
+    visited = [0] * (v + 1)
+    dq = deque([[s, 0]])
+
+    while dq:
+        s, cnt = dq.popleft()
+
+        if s in gh:
+            for n in gh[s]:
+                if n == g:
+                    return cnt + 1
+                if not visited[n]:
+                    visited[n] = 1
+                    dq.append([n, cnt + 1])
+
+    return 0
+
 
 test_case = int(input())
 
 for test in range(1, test_case + 1):
-    a, b = input().split()
-    cnt = a.count(b)
+    v, e = map(int, input().split())
+    gh = {}
+    for _ in range(e):
+        n1, n2 = map(int, input().split())
+        if gh.get(n1):
+            gh[n1].append(n2)
+        else:
+            gh[n1] = [n2]
+        if gh.get(n2):
+            gh[n2].append(n1)
+        else:
+            gh[n2] = [n1]
 
-    ans = len(a) - len(b) * (cnt) + cnt
+    s, g = map(int, input().split())
+    ans = bfs(s)
 
     print("#{} {}".format(test, ans))
