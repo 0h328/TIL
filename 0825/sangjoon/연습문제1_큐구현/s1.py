66ab37227cfb81f8dfac75c971bff7cbfff0e364
onshore <onshorecoding9@gmail.com>
2021-08-26 10:29:32 +0900

    210826: practice

--- 0825/sangjoon/연습문제1_큐구현/s1.py
+++ 0825/sangjoon/연습문제1_큐구현/s1.py
@@ -1,15 +1,20 @@
-# 문제 푼 시간
-# 풀이법: Count 사용
-import pathlib, sys
+"""
+문제1-2. 기본 Queue 구현 - 기본 구현 (내장 모듈 활용)
+ - 세 개의 데이터 1, 2, 3을 차례로 큐에 삽입
+ - 큐에서 세 개의 데이터를 차례로 꺼내어 출력
+  (1, 2, 3을 차례대로 출력)
+"""
 
-sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
+# 1. Queue 생성
 
-test_case = int(input())
+q = queue.Queue()
+print(q.empty())
 
-for test in range(1, test_case + 1):
-    a, b = input().split()
-    cnt = a.count(b)
+# 2. Queue에 데이터를 삽입
 
-    ans = len(a) - len(b) * (cnt) + cnt
+q.put(1)
+print(q.empty())
+print(q.qsize())
+print(q.qet())
 
-    print("#{} {}".format(test, ans))
+# 3. Queue에 삽입한 데이터를 출력(First-In-First-Out)
---
+++ 0825/sangjoon/연습문제2_BFS구현 copy 2/s1.py
@@ -0,0 +1,24 @@
+# 문제 푼 시간
+# 풀이법: Count 사용
+import collections
+import pathlib, sys
+
+
+sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
+
+def bfs(x:int, y: int):
+
+
+
+test_case = int(input())
+
+for test in range(1, test_case + 1):
+    n = int(input().split())
+    mp = [list(map(int, input().split())) for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if mp[i][j] == 2:
+                bfs(i, j)
+
+    print("#{} {}".format(test, ans))
