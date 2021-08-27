66ac059cae760d38aae4d96d7c8c5f2e3c0b9dc2
onshore <onshorecoding9@gmail.com>
2021-08-27 13:03:05 +0900

    0826: fix code 5102

--- 0826/sangjoon/5102_노드의거리/s1.py
+++ 0826/sangjoon/5102_노드의거리/s1.py
@@ -13,13 +13,12 @@ def bfs(s: int):
     while dq:
         s, cnt = dq.popleft()
 
-        if s in gh:
-            for n in gh[s]:
-                if n == g:
-                    return cnt + 1
-                if not visited[n]:
-                    visited[n] = 1
-                    dq.append([n, cnt + 1])
+        for n in grf[s]:
+            if n == g:
+                return cnt + 1
+            if not visited[n]:
+                visited[n] = 1
+                dq.append([n, cnt + 1])
 
     return 0
 
@@ -28,17 +27,12 @@ test_case = int(input())
 
 for test in range(1, test_case + 1):
     v, e = map(int, input().split())
-    gh = {}
+    grf = {i: [] for i in range(v + 1)}
+
     for _ in range(e):
         n1, n2 = map(int, input().split())
-        if gh.get(n1):
-            gh[n1].append(n2)
-        else:
-            gh[n1] = [n2]
-        if gh.get(n2):
-            gh[n2].append(n1)
-        else:
-            gh[n2] = [n1]
+        grf[n1].append(n2)
+        grf[n2].append(n1)
 
     s, g = map(int, input().split())
     ans = bfs(s)
