8cdf2705442ec126f7e3b3850f4ee6fd10f9fd93
onshore <onshorecoding9@gmail.com>
2021-08-27 18:11:19 +0900

    0827: solve im test practice

--- IM대비문제/sangjoon/4613_러시아국기같은깃발/s2.py
+++ IM대비문제/sangjoon/4613_러시아국기같은깃발/s2.py
@@ -1,48 +1,64 @@
 # 문제 푼 시간
-# 풀이법: dfs사용 # 실패
+# 풀이법: dfs -> 실패
+
+# 다시 고쳐야할 필요 있음
+
 import pathlib, sys
+from itertools import combinations
 
 sys.stdin = open(str(pathlib.Path(__file__).parent.absolute()) + "/input.txt")
 
 
 def dfs(line: int, cnt: int):
     global min_cnt
-    global start_blue
+    global end_white
     global end_blue
 
     if line == n - 1:
-        if cnt < min_cnt and start_blue:  # 파란이 칠해진 경우
+        if cnt < min_cnt and end_white:
             min_cnt = cnt
         return
 
-    if not start_blue:  # 위에서 부터 파란색이 칠해지기 전 가능 조합
+    if not end_white:
         color_lst = ["W", "B"]
-    elif not end_blue:  # 파란색이 칠해진 후 가능한 조합
+    elif not end_blue:
         color_lst = ["B", "R"]
-    else:  # 빨간색이 칠해진 후 가능 한 조합
+    else:
         color_lst = ["R"]
 
     for color in color_lst:
-        if color == "B":  # 파란색을 칠한 경우
-            start_blue = True
-        if color == "R":  # 빨간색을 칠한 경우
+        if color == "B":
+            end_white = True
+        if color == "R":
             end_blue = True
 
-        cnt_c = m - lst[line].count(color)  # 칠해야할 색상 개수
+        cnt_c = flag_dic[line][color]
         cnt += cnt_c
-        if cnt < min_cnt:  # 작을 경우에만 탐색
-            dfs(line + 1, cnt)
+        dfs(line + 1, cnt)
         cnt -= cnt_c
+
+        if color == "B":
+            end_white = False
+        if color == "R":
+            end_blue = False
 
 
 test_case = int(input())
 
 for test in range(1, test_case + 1):
     n, m = map(int, input().split())
-    lst = [list(input()) for _ in range(n)]
-    ans = m * 2 - lst[0].count("W") - lst[n - 1].count("R")
+    flag_dic = {i: {} for i in range(n)}
+
+    for i in range(n):
+        word = list(input())
+        flag_dic[i]["W"] = m - word.count("W")
+        flag_dic[i]["B"] = m - word.count("B")
+        flag_dic[i]["R"] = m - word.count("R")
+
+    print(flag_dic)
+    ans = flag_dic[0]["W"] + flag_dic[n - 1]["R"]
     min_cnt = m * n
-    start_blue, end_blue = False, False
+    end_white, end_blue = False, False
 
     dfs(1, ans)
 
