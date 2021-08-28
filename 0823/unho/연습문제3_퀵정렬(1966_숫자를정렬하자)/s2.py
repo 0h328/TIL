fbdc4ef7d46a2a38bf7504a3c5ed208a78988986
unho <djsgh94@naver.com>
2021-08-28 19:33:52 +0900

    0827 finish

--- 0823/unho/연습문제3_퀵정렬(1966_숫자를정렬하자)/s2.py
+++ 0823/unho/연습문제3_퀵정렬(1966_숫자를정렬하자)/s2.py
@@ -5,6 +5,39 @@ def partition(arr, start, end):
     pivot = arr[start]
     left = start + 1
     right = end
+    sign = True
+    while sign:
+        while left <= right and pivot >= arr[left]:
+            left += 1
+        while left <= right and pivot <= arr[right]:
+            right -= 1
+
+        if right < left:
+            sign = False
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    arr[start], arr[right] = arr[right], arr[start]
+    return right
+
+
+def quicksort(arr, start, end):
+    if start < end:
+        pivot = partition(arr, start, end)
+        quicksort(arr, start, pivot-1)
+        quicksort(arr, pivot+1, end)
+    return arr
+
+nums = list(map(int, input().split()))
+print(quicksort(nums, 0, len(nums)-1))
+
+
+
+
+'''
+def partition(arr, start, end):
+    pivot = arr[start]
+    left = start + 1
+    right = end
     done = False
     while not done:
         while left <= right and arr[left] <= pivot:
@@ -27,3 +60,4 @@ def quick_sort(arr, start, end):
 
 nums = list(map(int, input().split()))
 print(quick_sort(nums, 0, len(nums)-1))
+'''
