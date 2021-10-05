44b7b426be21f7431743d7eebec571c01ec97676
swsilver95 <dmstmddnjs@hanmail.net>
2021-10-05 18:02:01 +0900

    Finished 1005 problem set

--- 1001/swsilver95/1244_최대상금/s1.py
+++ 1001/swsilver95/1244_최대상금/s1.py
@@ -11,13 +11,18 @@ def dfs(cnt):
         return
 
     else:
+        # 두 자리를 모두 바꾸는 과정
         for i in range(M - 1):
             for j in range(i + 1, M):
+                # 두 자리를 바꿔서
                 numbers[i], numbers[j] = numbers[j], numbers[i]
+                # 숫자로 바꿔준다.
                 num = int(''.join(map(str, numbers)))
+                # 해당 숫자가 같은 교환 횟수에서 등장한적이 있다면 다시 할 필요 없음
                 if (num, cnt) not in visited:
                     visited.add((num, cnt))
                     dfs(cnt + 1)
+                # 바꾼 숫자 원상 복구
                 numbers[i], numbers[j] = numbers[j], numbers[i]
 
 
