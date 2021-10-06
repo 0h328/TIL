82694ef91358e2ff219fac350d8a487bc9b70d5b
unho-00 <djsgh94@naver.com>
2021-10-06 23:53:26 +0900

    finish

--- 1006/unho/연습문제_2/s1.py
+++ 1006/unho/연습문제_2/s1.py
@@ -4,28 +4,43 @@
 
 #4. 병합 정렬
 def merge(left, right):
-    for i in range(left, right+1):
-        for j in range(left+1, right+1-i):
-            if numbers[i] > numbers[j]:
-                numbers[i], numbers[j] = numbers[j], numbers[i]
-    return left
+    if len(left) == 0:
+        return right
+    if len(right) == 0:
+        return left
+    sorted_list = []
+    L = R = 0
 
-def partition(low, high):
-    if high - low <= 1:
-        print(low)
-        return low
+    while len(sorted_list) != len(left) + len(right):
+        if left[L] <= right[R]:
+            sorted_list.append(left[L])
+            L += 1
+        else:
+            sorted_list.append(right[R])
+            R += 1
 
-    mid = (low+high) // 2
+        if R == len(right):             
+            sorted_list += left[L:]     
+            break                      
 
-    left = partition(low, mid)
-    right = partition(mid, high)
+        if L == len(left):              
+            sorted_list += right[R:]
+            break
+    return sorted_list
+
+def partition(nums):
+    if len(nums) < 2:
+        return nums
+    
+    mid = len(nums) // 2
+    left = partition(nums[:mid])
+    right = partition(nums[mid:])
     
     return merge(left, right)
 
 numbers = [0, 55, 22, 33, 2, 1, 10, 26, 42]
 print(numbers)               # 정렬 전
-partition(0, len(numbers))
-print(numbers)    # 정렬 후
+print(partition(numbers))    # 정렬 후
 
 
 #5. 퀵 정렬
