414590114b0a5d3613ea1c95690b989bac6a8d5a
develohjh <oojh95@naver.com>
2021-10-06 20:25:15 +0900

    1006_1

--- 1005/oojh95/5202_화물도크/s1.py
+++ 1005/oojh95/5202_화물도크/s1.py
@@ -2,22 +2,7 @@ import sys
 from itertools import combinations
 sys.stdin = open('input.txt')
 
-def bfs(s, cnt):
-    global result
-    if s < N-1:
-        if not visited[arr[s][0]] and not visited[arr[s][1]-1]:
-            cnt += 1
-            if cnt > result:
-                result = cnt
-            for j in range(arr[s][0], arr[s][1]):
-                visited[j] = 1
-            bfs(s+1, cnt)
-
-        bfs(s+1, cnt)
-
-
 T = int(input())
-
 
 for tc in range(1, T+1):
     N = int(input())
@@ -26,16 +11,21 @@ for tc in range(1, T+1):
     result = 0
     while N > 0:
         tmp = list(combinations(arr, N))
-
-        for i in range(len(tmp[0])):
-            for j in range(tmp[0][i][0], tmp[0][i][1]):
-                if not visited[j]:
-                    visited[j] = 1
-                else:
-                    visited = [0] * 25
-                    N -= 1
+        for k in range(len(tmp)):
+            for i in range(len(tmp[k])):
+                for j in range(tmp[k][i][0], tmp[k][i][1]):
+                    if not visited[j]:
+                        visited[j] = 1
+                    else:
+                        visited = [0] * 25
+                        break
+                if not any(visited):
                     break
-        else:
-            result = N
+            else:
+                result = N
+                break
+        N -= 1
+        if result > 0:
             break
+
     print('#{} {}'.format(tc, result))
