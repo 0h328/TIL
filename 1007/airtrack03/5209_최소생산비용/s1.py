dd280f98e609e931fec7a2ff806e14ad87d2fbfc
sungryul <airtrack03@naver.com>
2021-10-12 17:30:16 +0900

    1012 hws Finished

--- 1007/airtrack03/5209_최소생산비용/s1.py
+++ 1007/airtrack03/5209_최소생산비용/s1.py
@@ -22,6 +22,19 @@ def calc_perm(n, k, total):
             total -= factory[k][i]
             chk[i] = 0
 
+def bit_masking(idx, s, visit): # idx:제품 번호, s: 중간 계산 결과, visit: 정수
+    global ans
+    if idx == N:    # if visit == (1 << N) - 1과 같은 조건문
+        if s < ans:
+            ans = s
+    if s >= ans:
+        return
+    for j in range(N):  # 공장을 고르자!
+        if visit & (1<<j):
+            continue
+        bit_masking(idx+1, s+factory[idx][j], visit | 1<<j)
+
+
 
 for tc in range(1, T + 1):
     N = int(input())
