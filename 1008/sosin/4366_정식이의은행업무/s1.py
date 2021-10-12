74278e040556480718af33a35f61e2ce70d1a02e
sosin <svstar94@naver.com>
2021-10-12 17:51:30 +0900

    1012 algorithm

---
+++ 1008/sosin/4366_정식이의은행업무/s1.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+for T in range(int(input())):
+    result = 0
+    two = input()
+    tri = input()
+    print(int(two, 2), int(tri, 3))
+    # 13 차이
+
+    print('#{} {}'.format((T+1), result))
+
+#1 14
