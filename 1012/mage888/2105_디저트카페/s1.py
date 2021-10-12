885aa7097366a48cefdca0be9b4dab47c9b8d3b1
mage888 <mage888@naver.com>
2021-10-12 18:00:49 +0900

    ㅅtemporarily push 1012

---
+++ 1012/mage888/2105_디저트카페/s1.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+dr = [1, 1, -1, -1]
+dc = [1, -1, 1, -1]
+
+def route(r, c):
+    pass
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    cafe = [list(map(int, input().split())) for _ in range(N)]
+
+
+
