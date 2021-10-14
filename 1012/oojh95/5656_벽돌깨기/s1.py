e441c11be363f99d2447b7f810ce97be037b24b5
develohjh <oojh95@naver.com>
2021-10-14 09:10:05 +0900

    1014_1

--- 1012/oojh95/5656_벽돌깨기/s1.py
+++ 1012/oojh95/5656_벽돌깨기/s1.py
@@ -3,17 +3,16 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def check(num, r, c):
-    k = num[r][c]
-    visited[r][c] = 1
+def check(num, r, c, k):
     for j in range(4):
         for l in range(1, k):
             nr = r + dr[j] * l
             nc = c + dc[j] * l
-            if 0 <= nr < H and 0 <= nc < W and num[nr][nc] > 0 and not visited[nr][nc]:
-                visited[nr][nc] = 1
-                if num[nr][nc] > 1:
-                    check(num, nr, nc)
+            if 0 <= nr < H and 0 <= nc < W and num[nr][nc] > 0:
+                tmp = num[nr][nc]
+                num[nr][nc] = 0
+                if tmp > 1:
+                    check(num, nr, nc, tmp)
 
 
 def work(nums, cnt, col):
@@ -28,27 +27,30 @@ def work(nums, cnt, col):
             result = sum_val
         sum_val = 0
         return
-
+    buck_down = num
     for h in range(H):
         if num[h][col]:
-            check(num, h, col)
-            for a in range(H):
-                for b in range(W):
-                    if visited[a][b]:
-                        num[a][b] = 0
-                        for c in range(a, 0, -1):
-                            num[c][b], num[c-1][b] = num[c-1][b], num[c][b]
-            visited = [[0] * W for _ in range(H)]
+            itmp = num[h][col]
+            num[h][col] = 0
+            check(num, h, col, itmp)
+            buck_down = [[0 for _ in range(W)] for _ in range(H)]
+            for d in range(W):
+                temp = []
+                for e in range(H):
+                    if num[e][d]:
+                        temp.append(num[e][d])
+                for j in range(len(temp)):
+                    buck_down[H - len(temp) + j][d] = temp[j]
             break
     for i in range(W):
-        work(num, cnt - 1, i)
+        work(buck_down, cnt - 1, i)
 
 
 T = int(input())
 for tc in range(1, T+1):
     N, W, H = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(H)]
-    visited = [[0]*W for _ in range(H)]
+
     result = 9*12*15
     sum_val = 0
     dr = [-1, 0, 1, 0]
