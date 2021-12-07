035b2e52b91d028512becfed7facc65736fde206
mage888 <mage888@naver.com>
2021-12-07 23:58:56 +0900

    11050 solved

---
+++ algorithm/baekjoon/Math/11050.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, K = map(int, input().split())
+
+def fact(N):
+    if N <= 1:
+        return 1
+
+    return N * fact(N-1)
+
+print(fact(N) // (fact(K) * fact(N-K)))
