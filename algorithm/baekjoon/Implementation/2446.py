62afbc9e78eb8668817a7fa31fcd52049e32fbe1
mage888 <mage888@naver.com>
2021-12-10 01:25:38 +0900

    2446 solved

---
+++ algorithm/baekjoon/Implementation/2446.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+
+for i in range(N, 0, -1):
+    print((N-i) * ' ' + (2*i - 1) * '*')
+for i in range(2, N+1):
+    print((N-i) * ' ' + (2*i - 1) * '*')
