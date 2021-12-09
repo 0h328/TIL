550e135e8c8ca41b2cd95f1d556cbae3ff09146e
mage888 <mage888@naver.com>
2021-12-10 01:29:27 +0900

    2522 solved

---
+++ algorithm/baekjoon/Implementation/2522.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+
+for i in range(1, N+1):
+    print((N-i) * ' ' + i * '*')
+for i in range(N-1, 0, -1):
+    print((N-i) * ' ' + i * '*')
