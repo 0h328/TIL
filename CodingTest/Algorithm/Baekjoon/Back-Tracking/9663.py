37e51be51119b758c245a9e4e1b0a0a80efdfbd2
mage888 <mage888@naver.com>
2021-12-13 09:45:38 +0900

    9663 solved

--- CodingTest/Algorithm/Baekjoon/Back-Tracking/9663.py
+++ CodingTest/Algorithm/Baekjoon/Back-Tracking/9663.py
@@ -1,27 +1,31 @@
+# 참고
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.stdin = open('input.txt')
 
-def is_queen(x):
-    for i in range(x):
-        if arr[x] == arr[i] or abs(arr[x] - arr[i]) == x - i:
+def check(n):
+    for i in range(n):
+        if row[n] == row[i] or abs(row[n] - row[i]) == n - i:
             return 0
     return 1
 
 
-def dfs(x):
+def dfs(n):
     global res
-
-    if x == N:
+    if n == N:
         res += 1
     else:
         for i in range(N):
-            arr[x] = i
-            if is_queen(x):
-                dfs(x+1)
+            row[n] = i
+            if check(n):
+                dfs(n + 1)
+
 
 N = int(input())
-arr = [0] * N
+row = [0] * N
 res = 0
 dfs(0)
 print(res)
+
+# 가라
+# answer = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596]
+# print(answer[int(input())])
