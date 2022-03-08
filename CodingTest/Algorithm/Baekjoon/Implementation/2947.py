ec1ca4b93b3372ce349eee6ea333a4f4c506577f
mage888 <mage888@naver.com>
2022-03-08 21:56:51 +0900

    2947 commit

---
+++ CodingTest/Algorithm/Baekjoon/Implementation/2947.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+nums = list(map(int, input().split()))
+
+while True:
+    for i in range(len(nums)-1):
+        if nums[i] > nums[i+1]:
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+            print(*nums)
+    if nums == sorted(nums):
+        break
