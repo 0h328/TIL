3c35bb88326ed026adfae4f263c5f13805ca8f1e
0h328 <yhkim03280@gmail.com>
2022-04-07 23:47:48 +0900

    1912 commit

---
+++ CodingTest/Algorithm/Baekjoon/Dynamic-Programming/1912.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+sum = [nums[0]]
+for i in range(len(nums) - 1):
+    sum.append(max(sum[i] + nums[i + 1], nums[i + 1]))
+
+print(max(sum))
