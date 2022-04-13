8ecdef8e93d4b5c8fd282daaadfa9eee37c3f896
0h328 <mage888@naver.com>
2022-04-13 23:54:11 +0900

    2490 commit

---
+++ CodingTest/Algorithm/Baekjoon/Implementation/2490.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+for _ in range(3):
+    x = list(map(int, input().split()))
+    if x.count(0) == 1:
+        print('A')
+    elif x.count(0) == 2:
+        print('B')
+    elif x.count(0) == 3:
+        print('C')
+    elif x.count(0) == 4:
+        print('D')
+    else:
+        print('E')
+
