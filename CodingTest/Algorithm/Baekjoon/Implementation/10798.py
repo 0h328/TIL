bdc0a01727b6f02abc9b14afdc5d4ed0b826d90d
0h328 <mage888@naver.com>
2022-04-23 23:58:32 +0900

    10798 commit

---
+++ CodingTest/Algorithm/Baekjoon/Implementation/10798.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+words = [input() for i in range(5)]
+
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]):
+            print(words[i][j], end='')
+
+
+
