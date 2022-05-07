a57c2e21995f2286f91fda127de2c42dad9beca1
0h328 <mage888@naver.com>
2022-05-07 23:53:14 +0900

    8979 commit

---
+++ CodingTest/Algorithm/Baekjoon/Implementation/8979.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, K = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+for i in range(N):
+    if arr[i][0] == K:
+        idx = i
+for i in range(N):
+    if arr[idx][1:] == arr[i][1:]:
+        print(i + 1)
+        break
