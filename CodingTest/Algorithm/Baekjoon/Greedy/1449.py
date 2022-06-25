9229491fa9fe5f4c03d025dcf1c89ab77071c6fd
0h328 <mage888@naver.com>
2022-06-25 23:51:55 +0900

    solve : boj 1449

---
+++ CodingTest/Algorithm/Baekjoon/Greedy/1449.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, L = map(int, input().split())
+s = list(map(int, input().split()))
+s.sort()
+start = s[0]
+end = s[0] + L
+cnt = 1
+for i in range(N):
+    if start <= s[i] < end:
+        continue
+    else:
+        start = s[i]
+        end = s[i] + L
+        cnt += 1
+        
+print(cnt)
