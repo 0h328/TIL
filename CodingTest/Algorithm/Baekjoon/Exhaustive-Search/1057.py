ce610087f8c885107a38544628f4a4f6ac9cd860
0h328 <mage888@naver.com>
2022-07-27 22:49:38 +0900

    solve : boj 1057

---
+++ CodingTest/Algorithm/Baekjoon/Exhaustive-Search/1057.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, Kim, Lim = map(int, input().split())
+cnt = 0
+while Kim != Lim:
+    Kim -= Kim // 2
+    Lim -= Lim // 2
+    cnt += 1
+
+print(cnt)
