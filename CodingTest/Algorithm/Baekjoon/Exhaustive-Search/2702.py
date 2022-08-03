cd8345dd36f3e486f2064dc3e7400f3491501ec6
0h328 <mage888@naver.com>
2022-08-03 22:08:49 +0900

    solve : boj 2702

---
+++ CodingTest/Algorithm/Baekjoon/Exhaustive-Search/2702.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+def lcm(a, b):
+    return int((a * b) / gcd(a, b))
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    else:
+        return gcd(b % a, a)
+
+T = int(input())
+for i in range(T):
+    a, b = map(int, input().split())
+    print(lcm(a, b), gcd(a, b))
