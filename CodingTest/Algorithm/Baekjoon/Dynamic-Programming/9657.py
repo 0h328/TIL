8b18654559befed6f9f9caf8620ac718243eab67
0h328 <mage888@naver.com>
2022-08-11 22:23:42 +0900

    solve : boj 9657

---
+++ CodingTest/Algorithm/Baekjoon/Dynamic-Programming/9657.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+dp = [0, 'Win', 'Lose', 'Win', 'Win'] + [0]*(N-4)
+
+if N <= 4:
+    pass
+else:
+    for i in range(5, N+1):
+        if 'Lose' in [dp[i-1], dp[i-3], dp[i-4]]:
+            dp[i] = 'Win'
+        else:
+            dp[i] = 'Lose'
+
+if dp[N] == 'Win':
+    print('SK')
+else:
+    print('CY')
