5724e6e3d265484d051bba158ce4dbcd47cba759
0h328 <mage888@naver.com>
2022-08-14 22:51:44 +0900

    solve : boj 1699

---
+++ CodingTest/Algorithm/Baekjoon/Dynamic-Programming/1699.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+
+dp = [0 for i in range(N+1)]
+square = [i * i for i in range(1, 317)]
+for i in range(1, N+1):
+    s = []
+    for j in square:
+        if j > i:
+            break
+        s.append(dp[i - j])
+    dp[i] = min(s) + 1
+
+print(dp[N])
