f218935a7575be621cc82ca1e6c6d1b7ba5abbc2
0h328 <mage888@naver.com>
2022-09-11 23:45:16 +0900

    solve : boj 9076

---
+++ CodingTest/Algorithm/Baekjoon/Sorting/9076.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for i in range(T):
+    N = list(map(int, input().split()))
+    N.remove(max(N))
+    N.remove(min(N))
+    if max(N) - min(N) >= 4:
+        print('KIN')
+    else:
+        print(sum(N))
