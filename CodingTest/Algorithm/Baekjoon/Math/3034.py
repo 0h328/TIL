ad91bd73bf495184687fb312777771cd04febc97
0h328 <mage888@naver.com>
2022-09-19 23:23:51 +0900

    solve : boj 3034

---
+++ CodingTest/Algorithm/Baekjoon/Math/3034.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, W, H = map(int, input().split())
+for i in range(N):
+    b = int(input())
+    l = (W**2 + H**2) ** 0.5
+    if b <= l:
+        print("DA")
+    else:
+        print("NE")
