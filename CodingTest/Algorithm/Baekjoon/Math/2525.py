8a08c796b4cbfc8d06495ee21141665e6758d9d6
0h328 <mage888@naver.com>
2022-09-30 21:25:33 +0900

    solve : boj 2525

---
+++ CodingTest/Algorithm/Baekjoon/Math/2525.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+A, B = map(int, input().split())
+timer = int(input())
+
+A += timer // 60
+B += timer % 60
+
+if B >= 60:
+    A += 1
+    B -= 60
+if A >= 24:
+    A -= 24
+
+print(A, B)
