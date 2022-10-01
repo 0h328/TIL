a5c4a56bdf6c9c1fbed783749707efa46072e924
0h328 <mage888@naver.com>
2022-10-01 23:29:08 +0900

    solve : boj 5543

---
+++ CodingTest/Algorithm/Baekjoon/Math/5543.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+a = 2000
+c = 2000
+for i in range(3):
+    b = int(input())
+    a = min(a, b)
+for i in range(2):
+    b = int(input())
+    c = min(c, b)
+    
+print(a + c - 50)
