f93e6a32afe808df935c71ac8f8cf5a2a3d42af1
0h328 <mage888@naver.com>
2022-10-16 18:32:15 +0900

    solve : boj 2845

---
+++ CodingTest/Algorithm/Baekjoon/Math/2845.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('input.txt')
+
+a, b = map(int, input().split())
+people = list(map(int, input().split()))
+party = a * b
+for i in people:
+    print(i - party, end=' ')
