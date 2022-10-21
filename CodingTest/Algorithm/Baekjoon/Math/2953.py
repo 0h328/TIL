bf2754d1c0d594081dfa4c9f7cb7c3a024a44069
0h328 <mage888@naver.com>
2022-10-21 22:26:49 +0900

    solve : boj 2953

---
+++ CodingTest/Algorithm/Baekjoon/Math/2953.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+total = []
+for _ in range(5):
+    score = list(map(int, input().split()))
+    total.append(sum(score))
+
+print(total.index(max(total))+1, max(total))
