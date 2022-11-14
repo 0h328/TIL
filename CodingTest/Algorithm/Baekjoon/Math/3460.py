50cef405f10cd1eacd405e78c91e72d53460e4d6
0h328 <mage888@naver.com>
2022-11-14 22:04:22 +0900

    solve : boj 3460

---
+++ CodingTest/Algorithm/Baekjoon/Math/3460.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for _ in range(T):
+    n = bin(int(input()))[2:]
+    for idx, val in enumerate(n[::-1]):
+        if val == '1':
+            print(idx, end=' ')
